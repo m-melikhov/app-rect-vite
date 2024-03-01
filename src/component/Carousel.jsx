14bc2ad69c6936bd27d15a6476bf01661615713f
Maxim Melikhov <melekhov25@gmail.com>
2024-03-01 15:37:44 +0200

    first commit

---
+++ src/component/Carousel.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Carousel } from "bootstrap";
+
+export default function MyCarousel() {
+  const myCarouselRef = useRef(null);
+
+  useEffect(() => {
+    if (myCarouselRef.current) {
+      new Carousel(myCarouselRef.current, {
+        interval: 1000,
+        touch: false
+      });
+    }
+  }, []);
+
+  return (
+    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" ref={myCarouselRef}>
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="src/img/1234.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="src/img/2345.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="src/img/1234.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  )
+}
